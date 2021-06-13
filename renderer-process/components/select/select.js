8d30b9de59a1f1020dfd8bcc9233c2730f15019b
JitkaM <zoa@seznam.cz>
2021-06-13 22:51:29 +0200

    style and refactoring

--- renderer-process/components/select/select.js
+++ renderer-process/components/select/select.js
@@ -8,6 +8,35 @@ export class Select extends HTMLElement{
         </select>`;   
 
     }
+
+    createYears(){
+       const select = document.querySelector('#year');
+        for(let i = new Date().getFullYear() - 80; i <= new Date().getFullYear() + 20; i++){
+            const option = document.createElement('option');
+            option.setAttribute('value', i);
+            option.innerText = i;
+            if(i == new Date().getFullYear()){
+                option.setAttribute('selected', 'selected');
+            }
+            select.appendChild(option);
+        }
+    }
+
+    createMonths(){
+        const select = document.querySelector('#month');
+        const months = ['leden', 'únor', 'březen', 'duben', 'květen', 'červen', 'červenec', 'srpen', 'září', 
+                        'říjen', 'listopad', 'prosinec'];
+        for(let i = 0; i < months.length; i++){
+            const option = document.createElement('option');
+            option.setAttribute('value', i);
+            option.innerText = months[i];
+            if(i == new Date().getMonth()){
+                option.setAttribute('selected', 'selected');
+            }
+            select.appendChild(option);
+        }
+    }
+    
   
 
 }
--- renderer-process/main.js
+++ renderer-process/main.js
@@ -20,46 +20,15 @@ const section = document.querySelector('section.main-content');
 const sectionSelect = document.querySelector('.selects');
 const selectYear = new Select('year');
 sectionSelect.appendChild(selectYear);
+selectYear.createYears(); 
 const selectMonth = new Select('month');
 sectionSelect.appendChild(selectMonth);
- 
-
-function createYears(){
-    const select = document.querySelector('#year');
-    for(let i = new Date().getFullYear() - 80; i <= new Date().getFullYear() + 20; i++){
-        const option = document.createElement('option');
-        option.setAttribute('value', i);
-        option.innerText = i;
-        if(i == new Date().getFullYear()){
-            option.setAttribute('selected', 'selected');
-        }
-        select.appendChild(option);
-    }
-}
-
-createYears(); 
-
-function createMonths(){
-    const select = document.querySelector('#month');
-    const months = ['leden', 'únor', 'březen', 'duben', 'květen', 'červen', 'červenec', 'srpen', 'září', 
-                    'říjen', 'listopad', 'prosinec'];
-    for(let i = 0; i < months.length; i++){
-        const option = document.createElement('option');
-        option.setAttribute('value', i);
-        option.innerText = months[i];
-        if(i == new Date().getMonth()){
-            option.setAttribute('selected', 'selected');
-        }
-        select.appendChild(option);
-    }
-}
-
-createMonths();
+selectMonth.createMonths(); 
 
 
 let currentYear = new Date().getFullYear(); 
 let currentMonth = new Date().getMonth();
-drawCalendar();
+drawCalendar(); 
 
  document.querySelector('#year').addEventListener('change', (event)  => {
     currentYear = parseInt(event.target.value);
